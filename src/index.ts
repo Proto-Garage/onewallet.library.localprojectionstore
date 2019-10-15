9a61d95a196019c5df6b617313252ce035710c6f
djansyle <djansyledjans@gmail.com>
2019-10-15 19:09:20 +0800

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+interface ProjectionAttributes {
+  id: string;
+  lastEvent: string;
+}
+
+export default class {
+  protected map: { [key: string]: {} }
+  public constructor() {
+    this.map = {};
+  }
+  public async initialize() {
+    return;
+  }
+
+  public async findById(id: string) {
+    return this.map[id];
+  }
+
+  public async upsert(params: ProjectionAttributes) {
+    this.map[params.id] = params;
+  }
+}
